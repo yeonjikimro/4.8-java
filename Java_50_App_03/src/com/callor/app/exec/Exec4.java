eed61fe1a0d0e80df2a9181f5c2d83a222c86991
yeonjikimro <duswlskfk42@naver.com>
2022-04-27 11:15:37 +0900

    4.27

--- Java_50_App_03/src/com/callor/app/exec/Exec4.java
+++ Java_50_App_03/src/com/callor/app/exec/Exec4.java
@@ -9,22 +9,20 @@ public class Exec4 {
 		 * 생성한 정수가 소수인지 판별하시오
 		 */
 		boolean flag;
+		flag = true;
 
 		int num = (int)(Math.random() * 100) + 1;
 		
 	
-			flag = true;
 			for (int j = 2; j<num; j++) {
 				if(num%j == 0) {
 					flag = false;
-				}
-				
+					System.out.println(num +" 값은 소수가 아니다");
+				}	
 			} if(flag == true) {
-				System.out.println(num);	
+				System.out.println(num + " 값은 소수이다");	
 			}
-			
-		
-	
+
 		}
 
 	}
