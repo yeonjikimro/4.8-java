8a244d0c74dce7bf967f0879fe10866e918c56ee
yeonjikimro <duswlskfk42@naver.com>
2022-04-29 17:18:55 +0900

    4.29

--- Java_50_App_05/src/com/callor/app/model/ScoreVO.java
+++ Java_50_App_05/src/com/callor/app/model/ScoreVO.java
@@ -4,14 +4,44 @@ import com.callor.app.line.Line;
 
 public class ScoreVO {
 	
-	public static void score() {
-		
-			
-		System.out.println(Line.dLine(50));
-		System.out.println(Line.sLine(50));
-		
-		
-		
+	private int int_num;
+	private int int_Kor;
+	private int int_Eng;
+	private int int_Math;
+	public int getInt_num() {
+		return int_num;
 	}
+	public void setInt_num(int int_num) {
+		this.int_num = int_num;
+	}
+	public int getInt_Kor() {
+		return int_Kor;
+	}
+	public void setInt_Kor(int int_Kor) {
+		this.int_Kor = int_Kor;
+	}
+	public int getInt_Eng() {
+		return int_Eng;
+	}
+	public void setInt_Eng(int int_Eng) {
+		this.int_Eng = int_Eng;
+	}
+	public int getInt_Math() {
+		return int_Math;
+	}
+	public void setInt_Math(int int_Math) {
+		this.int_Math = int_Math;
+	}
+	@Override
+	public String toString() {
+		return "ScoreVO [int_num=" + int_num + ", int_Kor=" + int_Kor + ", int_Eng=" + int_Eng + ", int_Math="
+				+ int_Math + "]";
+	}
+	
+	
+	
+	
+		
+	
 
 }
--- Java_50_App_05/src/com/callor/app/service/ScoreService.java
+++ Java_50_App_05/src/com/callor/app/service/ScoreService.java
@@ -6,14 +6,47 @@ import java.util.List;
 import com.callor.app.model.ScoreVO;
 
 public class ScoreService {
+	/*
+	 * 1. 학생의 국어, 영어, 수학, 점수를 저장할 model.ScoreVO 클래스 작성
+	 * 2. service.Scoreservice 클래스를 작성
+	 * 3. List<ScoreVO>리스트를 선언하고
+	 * 4. makeScore() method 에서 각 과목에 50~100 까지 임의의 정수를 생성하여 저장
+	 * 5. 점수 List 를 20개 생성
+	 * 6. scorePrint() method 에서 성적 리스트를 출력
+	 */
+
 	
-	public static void main(String[] args) {
-		
-		List<Integer> ScoreVO = new ArrayList<>();
-		
+	private List<ScoreVO> scList;
+	public ScoreService() {
+		scList = new ArrayList<>();
 	}
-	
-	public int makeScore() {
+	public void makeScore() {
+
+		for(int i = 0; i < 20; i++) {
+			ScoreVO scVO = new ScoreVO();
+			scVO.setInt_num(i + 1);
+			int kor	= (int)(Math.random() * 50) + 51;
+			int eng	= (int)(Math.random() * 50) + 51;
+			int math= (int)(Math.random() * 50) + 51;
+			scVO.setInt_Kor(kor);
+			scVO.setInt_Eng(eng);
+			scVO.setInt_Math(math);
+			scList.add(scVO);
+			
+
+		}
+	}
+
+		public void printScore() {
+			for(ScoreVO scVO : scList) {
+				System.out.printf("%d\t%d\t%d\t%d\n",
+							scVO.getInt_num(),
+							scVO.getInt_Kor(),
+							scVO.getInt_Eng(),
+							scVO.getInt_Math()
+							);
+			}
+		
 		
 	}
 
