d4e2c0cd1059d378a904abf8658f75ecc7376ccd
yeonjikimro <101682172+yeonjikimro@users.noreply.github.com>
2022-04-30 18:35:59 +0900

    Update ScoreVO.java

--- Java_50_App_05/src/com/callor/app/model/ScoreVO.java
+++ Java_50_App_05/src/com/callor/app/model/ScoreVO.java
@@ -5,39 +5,66 @@ import com.callor.app.line.Line;
 public class ScoreVO {
 	
 	private int int_num;
-	private int int_Kor;
-	private int int_Eng;
-	private int int_Math;
+	private int int_kor;
+	private int int_eng;
+	private int int_math;
+	private int int_total;
+	private int int_avg;
+	
+	
 	public int getInt_num() {
 		return int_num;
 	}
 	public void setInt_num(int int_num) {
 		this.int_num = int_num;
 	}
-	public int getInt_Kor() {
-		return int_Kor;
+	public int getInt_kor() {
+		return int_kor;
 	}
-	public void setInt_Kor(int int_Kor) {
-		this.int_Kor = int_Kor;
+	public void setInt_kor(int int_kor) {
+		this.int_kor = int_kor;
 	}
-	public int getInt_Eng() {
-		return int_Eng;
+	public int getInt_eng() {
+		return int_eng;
 	}
-	public void setInt_Eng(int int_Eng) {
-		this.int_Eng = int_Eng;
+	public void setInt_eng(int int_eng) {
+		this.int_eng = int_eng;
 	}
-	public int getInt_Math() {
-		return int_Math;
+	public int getInt_math() {
+		return int_math;
 	}
-	public void setInt_Math(int int_Math) {
-		this.int_Math = int_Math;
-	}
-	@Override
-	public String toString() {
-		return "ScoreVO [int_num=" + int_num + ", int_Kor=" + int_Kor + ", int_Eng=" + int_Eng + ", int_Math="
-				+ int_Math + "]";
+	public void setInt_math(int int_math) {
+		this.int_math = int_math;
 	}
 	
+	
+	public int getInt_total() {
+		return int_total;
+	}
+	public void setInt_total(int int_total) {
+		this.int_total = int_total;
+	}
+	public int getInt_avg() {
+		
+		return int_avg;
+	}
+	public void setInt_avg(int int_avg) {
+		this.int_avg = int_avg;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "ScoreVO [int_num=" + int_num + ", int_kor=" + int_kor + ", int_eng=" + int_eng + ", int_math="
+				+ int_math + ", int_total=" + int_total + ", int_avg=" + int_avg + "]";
+	}
+	
+	
+	
+	
+	
+
 	
 	
 	
